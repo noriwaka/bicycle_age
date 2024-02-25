7b108f3352431884ffd350e44d3ca44c55558f7b
noriwaka <airsoft.manatsu@gmail.com>
2024-02-25 06:16:53 +0000

    before migration

---
+++ database/migrations/2024_02_25_150926_create_parts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('bicycle_id');
+            $table->string('name');
+            $table->unsignedBigInteger('mileage');
+            $table->timestamps();
+            
+            //外部キー制約
+            $table->foreign('bicycle_id')->references('id')->on('bicycles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parts');
+    }
+};
