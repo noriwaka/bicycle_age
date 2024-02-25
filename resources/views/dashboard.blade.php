425895fb2492e040bbcab28f78afb34587f68cef
noriwaka <airsoft.manatsu@gmail.com>
2024-02-25 05:22:30 +0000

    blade一部完了

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,25 +1,19 @@
 @extends('layouts.app')
 
-@section('content2')
-    <div class="flex justify-center">
-        <form method="POST" action="{{ route('register') }}" class="w-1/2">
-            @csrf
-                <h2 class="text-center font-medium my-4">メールアドレスとパスワードを入力してログイン</h2>
-                
-                <div class="form-control my-4">
-                <label for="email" class="label">
-                    <span class="label-text font-medium">メールアドレス</span>
-                </label>
-                <input type="email" name="email" class="input input-bordered w-full">
-            </div>
+@section('content')
 
-            <div class="form-control my-4">
-                <label for="password" class="label">
-                    <span class="label-text font-medium">パスワード</span>
-                </label>
-                <input type="password" name="password" class="input input-bordered w-full">
+@if(Auth::check())
+
+
+@else
+    <div class="prose hero bg-base-200 mx-auto max-w-full rounded">
+        <div class="hero-content text-center my-10">
+            <div class="max-w-md mb-10">
+                <h2>サービスを利用するためにはログインして下さい</h2>
+                    {{-- ログインページへのリンク --}}
+                <a class="btn btn-primary normal-case w-full mt-8" href="{{ route('login') }}">ログインページへ</a>
             </div>
-                <button type="submit" class="btn btn-primary normal-case w-full mt-8" href="{{ route('login') }}">ログイン</a>
-        </form>
+        </div>
     </div>
-@endsection('content2')
+@endif
+@endsection('content')
